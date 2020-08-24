a17ca4dc7f2ce9a9fad22cb84e9153a6ce48acf5
kritical0613 <kulishan0613@gmail.com>
2020-08-24 18:08:46 -0400

    Update css(addpayment button)

--- Frontend/src/components/common/WalletHeader.js
+++ Frontend/src/components/common/WalletHeader.js
@@ -14,7 +14,7 @@ const WalletHeader = ({ title, subtitle, className, ...attrs }) => {
   return (
     <Col xs="12" sm="12" className="wallet-page-header-class">
       <h3>{title}</h3>
-      <Button>Add payment method</Button>
+      <Button className="btn-add-payment">Add payment method</Button>
     </Col>
   )
 };
