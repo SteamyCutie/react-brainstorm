340223f427bef0398feb361e1a99c986a5a5a4d6
kritical0613 <kulishan0613@gmail.com>
2020-09-10 08:16:01 +0200

    Add seeders

--- Backend/database/seeds/WalletSeeder.php
+++ Backend/database/seeds/WalletSeeder.php
@@ -12,7 +12,18 @@ class WalletSeeder extends Seeder
     public function run()
     {
         $items = [
-            [],
+            ['id' => 1, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 50, 'status' => 1],
+            ['id' => 2, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 50, 'status' => 1],
+            ['id' => 3, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 78, 'status' => 1],
+            ['id' => 4, 'user_id' => 2, 'lId' => "123asbcdef", 'sName' => "Dulce Dorwart", 'conferenceTime' => "01h 25min", 'amount' => 90, 'status' => 2],
+            ['id' => 5, 'user_id' => 2, 'lId' => "123asbcdef", 'sName' => "Dulce Dorwart", 'conferenceTime' => "01h 25min", 'amount' => 40, 'status' => 1],
+            ['id' => 6, 'user_id' => 2, 'lId' => "123asbcdef", 'sName' => "Dulce Dorwart", 'conferenceTime' => "01h 25min", 'amount' => 45, 'status' => 2],
+            ['id' => 7, 'user_id' => 2, 'lId' => "123asbcdef", 'sName' => "Dulce Dorwart", 'conferenceTime' => "01h 25min", 'amount' => 62, 'status' => 1],
+            ['id' => 8, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 120, 'status' => 1],
+            ['id' => 9, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 30, 'status' => 1],
+            ['id' => 10, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 70, 'status' => 3],
+            ['id' => 11, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 50, 'status' => 3],
+            ['id' => 12, 'user_id' => 1, 'lId' => "123asbcdef", 'sName' => "Leo Septimus", 'conferenceTime' => "01h 25min", 'amount' => 50, 'status' => 3],
         ];
 
         foreach($items as $item)
