14bc3dd7bbfcc28cab72321d5f0ca723c9b97134
kritical0613 <kulishan0613@gmail.com>
2020-09-12 01:08:28 +0900

    # integrate logo with api

--- Frontend/src/components/layout/MainNavbar/NavbarNav/UserActions.js
+++ Frontend/src/components/layout/MainNavbar/NavbarNav/UserActions.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getuserinfo } from '../../../../api/api';
 import {
   Dropdown,
   DropdownToggle,
@@ -15,11 +16,31 @@ export default class UserActions extends React.Component {
     super(props);
 
     this.state = {
+      avatar: '',
       visible: false
     };
 
     this.toggleUserActions = this.toggleUserActions.bind(this);
   }
+
+  componentWillMount() {
+    this.getUserInformation();
+  }
+
+  getUserInformation = async() => {
+    try {
+      
+      const result = await getuserinfo({email: localStorage.getItem('email')});
+      if (result.data.result == "success") {
+        this.setState({avatar: result.data.data.avatar});
+      } else {
+        this.showFail();
+      }
+    } catch(err) {
+      
+    };
+  }
+
 
   toggleUserActions() {
     this.setState({
@@ -38,8 +59,9 @@ export default class UserActions extends React.Component {
         <DropdownToggle caret tag={NavLink} className="text-nowrap px-3">
           <img
             className="user-avatar rounded-circle mr-2"
-            src={require("./../../../../images/avatars/0.jpg")}
+            src={this.state.avatar}
             alt="User Avatar"
+            style={{height: '2.5rem'}}
           />{" "}
         </DropdownToggle>
         <Collapse tag={DropdownMenu} right small open={this.state.visible}>
