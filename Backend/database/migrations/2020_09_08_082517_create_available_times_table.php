c131165e65d79b7c4e7d1ff201d82dfb379a676d
kritical0613 <kulishan0613@gmail.com>
2020-09-22 11:52:54 +0200

    fixed some issues available timezone.

--- Backend/database/migrations/2020_09_08_082517_create_available_times_table.php
+++ Backend/database/migrations/2020_09_08_082517_create_available_times_table.php
@@ -6,32 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAvailableTimesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('available_times', function (Blueprint $table) {
-            $table->id();
-            $table->bigInteger("user_id")->unsigned();
-            $table->string('day_of_week');
-            $table->integer('fromTime');
-            $table->integer('toTime');
-
-            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('availabe_times');
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('available_times', function (Blueprint $table) {
+      $table->id();
+      $table->bigInteger("user_id")->unsigned();
+      $table->string('day_of_week');
+      $table->integer('fromTime');
+      $table->integer('toTime');
+      $table->integer('status');
+      $table->string('timezone');
+      $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+      $table->timestamps();
+    });
+  }
+  
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('availabe_times');
+  }
 }
