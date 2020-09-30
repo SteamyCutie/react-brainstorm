12d6899f2e9473fe1c9e13f501aea002c5736dd9
kritical0613 <kulishan0613@gmail.com>
2020-09-30 16:50:22 +0200

    Modify room call feature

--- Frontend/src/views/RoomCall.js
+++ Frontend/src/views/RoomCall.js
@@ -8,39 +8,48 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 
+import DeclineImg from '../images/call-decline.svg'
+import { Height } from "@material-ui/icons";
+
 const PARTICIPANT_MAIN_CLASS = 'participant main';
 const PARTICIPANT_CLASS = 'participant';
 
 var name;
-var participants = {};
+var participants = [];
 
 var room_ws = null;
 
-function Participant(name, isLocalVideo) {
-	this.name = name;
+function Participant(user_info, isLocalVideo) {
+  this.user_name = user_info.user_name;
+	this.user_id = user_info.user_id;
+	this.user_avatar = user_info.user_avatar;
+	this.user_type = user_info.user_type;
+
 	var container = document.createElement('div');
 	container.className = isPresentMainParticipant() ? PARTICIPANT_CLASS : PARTICIPANT_MAIN_CLASS;
-	container.id = name;
-	var span = document.createElement('span');
+	container.id = this.user_id;
+  var span = document.createElement('span');
+  var avatar = document.createElement('img');
   var video = document.createElement('video');
   var rtcPeer;
 
 	container.appendChild(video);
-	container.appendChild(span);
-	document.getElementById('room-video-container').appendChild(container);
-
-	
+  container.appendChild(span);
+  container.appendChild(avatar);
+  document.getElementById('room-video-container').appendChild(container);
 
 	video.id = 'video-' + name;
 	video.autoplay = true;
   video.controls = false;
+
+  avatar.src = this.user_avatar;
+	avatar.style = 'height: 40px; width: 40px;';
 
   if(isLocalVideo) {
     container.classList.add("local-video");
   } else {
     container.classList.add("room-video-display-none");
     container.classList.add("participant-video");
-    span.appendChild(document.createTextNode(name));
   }
 
 	this.getElement = function() {
@@ -58,10 +67,11 @@ function Participant(name, isLocalVideo) {
 	this.offerToReceiveVideo = function(error, offerSdp, wp){
 		if (error) return console.error ("sdp offer error")
 		console.log('Invoking SDP offer callback function');
-		var msg =  { id : "receiveVideoFrom",
-				sender : name,
-				sdpOffer : offerSdp
-			};
+		var msg =  {
+			id : "receiveVideoFrom",
+			sender : this.user_id,
+			sdpOffer : offerSdp
+		};
 		this.sendMessage(msg);
 	}
 
@@ -72,7 +82,7 @@ function Participant(name, isLocalVideo) {
     var message = {
       id: 'onIceCandidate',
       candidate: candidate,
-      name: name
+      user_id: this.user_id,
     };
     this.sendMessage(message);
 	}
@@ -100,7 +110,7 @@ export default class RoomCall extends React.Component {
       errorMsg: '',
       code: '',
       participants: [], 
-      selfName: '', 
+      self_info: {}, 
       roomName: '', 
       fullScreen: false, 
     }
@@ -109,7 +119,7 @@ export default class RoomCall extends React.Component {
   }
 
   componentWillMount() {
-    this.setWebsocket('wss://' + '192.168.136.130:8443' + '/groupcall');
+    this.setWebsocket('wss://' + '192.168.136.129:8443' + '/groupcall');
   }
 
   /******************************** Group Call Start ************************/
@@ -138,7 +148,7 @@ export default class RoomCall extends React.Component {
         that.receiveVideoResponse(parsedMessage);
         break;
       case 'iceCandidate':
-        participants[parsedMessage.name].rtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+        participants[parsedMessage.user_id].rtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
           if (error) {
           console.error("Error adding candidate: " + error);
           return;
@@ -154,36 +164,45 @@ export default class RoomCall extends React.Component {
   register() {
     
     const rand_num = Math.floor(Math.random() * 101).toString();
-    name = rand_num;
-    var room = 'aaa';
-  
-    this.setState({
-      selfName: rand_num, 
-      roomName: room, 
-    })
-  
-    var message = {
-      id : 'joinRoom',
-      name : name,
-      
-      room : room,
+
+    var info = {
+      room : "aaa",
+      user_id: rand_num + "@gmail.com", 
+      user_name: "Full Developer: " + rand_num, 
+      user_avatar: "https://brainshares.s3-us-west-2.amazonaws.com/1599807220_517526_aaa.png", 
+      user_type: 1, 
     }
-    this.sendMessage(message);
+
+    this.setState({
+      self_info: info, 
+      roomName: rand_num, 
+    }, () => {
+      var message = {
+        id : 'joinRoom',
+        room : this.state.self_info.room,
+        user_id: this.state.self_info.user_id, 
+        user_name: this.state.self_info.user_name, 
+        user_avatar: this.state.self_info.user_avatar, 
+        user_type: this.state.self_info.user_type, 
+      }
+      this.sendMessage(message);
+    })
   }
   
   onNewParticipant(request) {
     const {participants} = this.state;
     let temp = participants;
-    temp.push(request.name);
-
+    temp.push(request.user_info);
     this.setState({
       participants: temp, 
+    }, () => {
+      this.receiveVideo(request.user_info);
     })
-    this.receiveVideo(request.name);
+
   }
   
   receiveVideoResponse(result) {
-    participants[result.name].rtcPeer.processAnswer (result.sdpAnswer, function (error) {
+    participants[result.user_id].rtcPeer.processAnswer (result.sdpAnswer, function (error) {
       if (error) return console.error (error);
     });
   }
@@ -211,8 +230,8 @@ export default class RoomCall extends React.Component {
       }
     };
     
-    var participant = new Participant(this.state.selfName, true);
-    participants[this.state.selfName] = participant;
+    var participant = new Participant(this.state.self_info, true);
+    participants[this.state.self_info.user_id] = participant;
     var video = participant.getVideoElement();
   
     var options = {
@@ -234,7 +253,7 @@ export default class RoomCall extends React.Component {
     })
     
     msg.data.forEach(this.receiveVideo);
-    document.getElementById(this.state.participants[this.state.participants.length - 1]).classList.remove("room-video-display-none");
+    // document.getElementById(this.state.participants[this.state.participants.length - 1]).classList.remove("room-video-display-none");
   }
   
   leaveRoom() {
@@ -251,21 +270,21 @@ export default class RoomCall extends React.Component {
   
   receiveVideo(sender) {
     var participant = new Participant(sender, false);
-    participants[sender] = participant;
+    participants[sender.user_id] = participant;
     var video = participant.getVideoElement();
     const that = this;
   
     var options = {
-        remoteVideo: video,
-        onicecandidate: participant.onIceCandidate.bind(participant)
-      }
+      remoteVideo: video,
+      onicecandidate: participant.onIceCandidate.bind(participant)
+    }
   
     participant.rtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
-        function (error) {
-          if(error) {
-            return console.error(error);
-          }
-          this.generateOffer (participant.offerToReceiveVideo.bind(participant));
+      function (error) {
+        if(error) {
+          return console.error(error);
+        }
+        this.generateOffer (participant.offerToReceiveVideo.bind(participant));
     });
   }
   
@@ -279,9 +298,9 @@ export default class RoomCall extends React.Component {
       participants: filteredAry, 
     })
 
-    var participant = participants[request.name];
+    var participant = participants[request.user_id];
     participant.dispose();
-    delete participants[request.name];
+    delete participants[request.user_id];
   }
   
   sendMessage(message) {
@@ -302,60 +321,33 @@ export default class RoomCall extends React.Component {
 
   handleListItemClick(param) {
     this.state.participants.map((participant) => {
-      if(param === participant) {
-        document.getElementById(participant).classList.remove("room-video-display-none");
+      if(param === participant.user_id) {
+        document.getElementById(participant.user_id).classList.remove("room-video-display-none");
       } else {
-        document.getElementById(participant).classList.add("room-video-display-none");
+        document.getElementById(participant.user_id).classList.add("room-video-display-none");
       }
       
     })
   }
 
   toggleFullScreen() {
-    const element = document.getElementById("room-container");
 
-    if(!this.state.fullScreen) {
-      console.log("11111111111111111111111111111111")
-      if (element.requestFullscreen) {
-        element.requestFullscreen();
-      } else if (element.mozRequestFullScreen) { /* Firefox */
-        element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-        element.webkitRequestFullscreen();
-      } else if (element.msRequestFullscreen) { /* IE/Edge */
-        element.msRequestFullscreen();
-      }
-    } else {
-      console.log("2222222222222222222222222222222222")
-      element.webkitExitFullscreen();
-      if (element.exitFullscreen) {
-        element.exitFullscreen();
-      } else if (element.mozCancelFullScreen) { /* Firefox */
-        element.mozCancelFullScreen();
-      } else if (element.webkitExitFullscreen) { /* Chrome, Safari and Opera */
-        element.webkitExitFullscreen();
-      } else if (element.msExitFullscreen) { /* IE/Edge */
-        element.msExitFullscreen();
-      }
-    }
-
-    this.setState({
-      fullScreen: !this.state.fullScreen, 
-    })
   }
 
   render() {
     return (
       <div className="room-container" id="room-container">
-        <Col xl="9">
-          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.leaveRoom()}>Leave Room</Button>
-          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.register()}>Join Room</Button>
-          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.toggleFullScreen()}>Full Screen</Button>
-          <div className="room-video-container" id="room-video-container">
-
-          </div>
+        {/* <Col xl="9">
+          <Row>
+            <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.leaveRoom()}>Leave Room</Button>
+            <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.register()}>Join Room</Button>
+            <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.toggleFullScreen()}>Full Screen</Button>
+          </Row>
+          <Row className="room-video-container" id="room-video-container">
+            
+          </Row>
           <div id="room-chat-area" className="room-chat-area">
-            <FormGroup>
+            <FormGroup className="no-bottom-margin">
               <FormTextarea placeholder="Please input" className="chat-history"/>
               <FormInput placeholder="Input.." className="chat-input"/>
             </FormGroup>
@@ -384,7 +376,47 @@ export default class RoomCall extends React.Component {
               })}
             </List>
           </div>
-        </Col>
+        </Col> */}
+        <div className="room-button-container">
+          
+        </div>
+        <div className="room-video-container center" id="room-video-container">
+            
+        </div>
+        <div id="room-member" width="20%" className="room-member">
+          <List dense >
+            {this.state.participants.map((participant, key) => {
+              // const labelId = {participant.user_id};
+              return (
+                <ListItem 
+                  key={participant.user_id} 
+                  button
+                  onClick={() => this.handleListItemClick(participant.user_id)}
+                >
+                  <ListItemAvatar>
+                    <Avatar
+                      alt={participant.user_name}
+                      src={participant.user_avatar}
+                    />
+                  </ListItemAvatar>
+                  <ListItemText id={participant.user_id} primary={participant.user_name} />
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
+        {/* <div id="room-small-video-container" className="room-small-video-container">
+
+        </div> */}
+        <div className="room-control-container">
+          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.leaveRoom()}>Leave Room</Button>
+          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.register()}>Join Room</Button>
+          <Button className="live-forum-header-button" style={{marginRight: "10px"}} onClick={() => this.toggleFullScreen()}>Full Screen</Button>
+          
+          <Button className="btn-room-call-decline" style={{marginRight: "10px"}} onClick={() => this.toggleFullScreen()}>
+            <img src={DeclineImg} placeholder="Phone" style={{height: "60px", width: "60px"}} alt="Decline"/>
+          </Button>
+        </div>
       </div>
     )
   }
