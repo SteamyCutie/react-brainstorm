b988e9238821633c9f45ba72aa88883c7c67e490
kritical0613 <kulishan0613@gmail.com>
2020-10-24 13:38:18 +0000

    fix issues

--- Frontend/src/components/common/Loginbygoogle.js
+++ Frontend/src/components/common/Loginbygoogle.js
@@ -36,7 +36,21 @@ export class Loginbygoogle extends Component {
       const result = await signbysocial(googleresponse);
       if (result.data.result === "success") {
         this.setState({user: result.data.user});
-        this.createAWSKinesisChannel();
+        if (result.data.logged_type === "signup") {
+          this.createAWSKinesisChannel();
+        } else {
+          localStorage.setItem('email', result.data.user.email);
+          localStorage.setItem('user_id', result.data.user.id);
+          localStorage.setItem('avatar', result.data.user.avatar);
+          localStorage.setItem('user_name', result.data.user.name);
+          localStorage.setItem('pay_verified', result.data.user.pay_verified);
+          localStorage.setItem('channel_name', result.data.user.channel_name);
+          localStorage.setItem('user-type', (result.data.user.is_mentor === 1 ? true : false));
+          if (result.data.user.is_mentor === 1)
+            window.location.href = "/mentorDashboard";
+          else
+            window.location.href = "/studentDashboard";
+        }
       } else {
         errorOccur(result.data.message);
       }
