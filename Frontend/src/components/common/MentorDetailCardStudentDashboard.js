6a9b481d6aa7295889139e3082337bbf690b6dc5
kritical0613 <kulishan0613@gmail.com>
2020-11-06 20:16:10 +0400

    fixed some issues, mentor and student dashboard subscription and unsubscription.

--- Frontend/src/components/common/MentorDetailCardStudentDashboard.js
+++ Frontend/src/components/common/MentorDetailCardStudentDashboard.js
@@ -11,6 +11,8 @@ import Lightening from "../../images/Lightening.svg";
 import SubscriperImg from "../../images/Users.svg";
 import Clock from "../../images/Clock.svg";
 import defaultavatar from "../../images/avatar.jpg";
+import { unsubscription } from '../../api/api';
+import LoadingModal from "../../components/common/LoadingModal";
 
 class MentorDetailCardStudentDashboard extends React.Component {
   constructor(props) {
@@ -21,6 +23,7 @@ class MentorDetailCardStudentDashboard extends React.Component {
       teaches: [],
       subscriptionOpen: false,
       addnewcardModal: false,
+      loading: false,
     }
   }
 
@@ -38,15 +41,15 @@ class MentorDetailCardStudentDashboard extends React.Component {
   }
 
   readMore() {
-    this.setState({more: true});
+    this.setState({ more: true });
   }
 
   readLess() {
-    this.setState({more: false});
+    this.setState({ more: false });
   }
 
-  toggle_unsubscribe() {    
-    this.setState({      
+  toggle_unsubscribe() {
+    this.setState({
       subscriptionOpen: !this.state.subscriptionOpen
     });
   }
@@ -64,118 +67,155 @@ class MentorDetailCardStudentDashboard extends React.Component {
     });
   }
 
+  handleUnSub = async (id) => {
+    let param = {
+      mentor_id: id,
+      email: localStorage.getItem('email')
+    };
+
+    try {
+      this.setState({ loading: true });
+      const result = await unsubscription(param);
+      if (result.data.result === "success") {
+      } else if (result.data.result === "warning") {
+        this.showWarning(result.data.message);
+      } else {
+        if (result.data.message === "Token is Expired") {
+          this.showFail(result.data.message);
+          this.signout();
+        } else if (result.data.message === "Token is Invalid") {
+          this.showFail(result.data.message);
+          this.signout();
+        } else if (result.data.message === "Authorization Token not found") {
+          this.showFail(result.data.message);
+          this.signout();
+        } else {
+          this.showFail(result.data.message);
+        }
+      }
+      this.setState({ loading: false });
+    } catch (err) {
+      this.setState({ loading: false });
+      this.showFail("Something Went wrong");
+    };
+  }
+
   actionSuccess() {
   }
-  
+
   render() {
-    const {id, name, avatar, tag_name, is_mentor, status, description, hourly_price, instant_call, video_url, average_mark, share_info, sub_count, sub_plan_fee } = this.props.mentorData;
-    const { subscriptionOpen } = this.state;
+    const { id, name, avatar, tag_name, is_mentor, status, description, hourly_price, instant_call, video_url, average_mark, share_info, sub_count, sub_plan_fee, sub_id } = this.props.mentorData;
+    const { subscriptionOpen, loading } = this.state;
 
     return (
       <>
-      <div className="mentor-detail-card">
-        <div style={{position: "relative"}} className="mentor-detail-avatar">
-          {avatar && <img src={avatar} alt={name} className="mentor-detail-avatar-img" />}
-          {!avatar && <img src={defaultavatar} alt={name} className="mentor-detail-avatar-img" />}
-          {
-            status === 1 && <div className="carousel-component-online-class"></div>
-          }
-        </div>
-        <div className="mentor-detail-desc">
-          <Row className="metor-detail-name-score">
-            <div className="mentor-detail-name">{name}</div>
-            {is_mentor ? <div><img src={StarIcon} alt="star-icon" className="mentor-detail-score"/>{average_mark}</div> : null}
-          </Row>
-          {is_mentor ? 
-            <Row className="mentor-detail-subject-tag">
-              <h5 className="tag-title mentor-detail-subject-title">Teaches: </h5>
-              {tag_name && tag_name.map((teach, idx) => {
-                if (idx < 3)
-                  return <p key={idx} className="brainsshare-tag" title={teach}>{teach}</p>;
-                else if (idx === 3)
-                  return <p key={idx}>{tag_name.length - 3} more</p>
-                else 
-                  return <></>;
-              })}
-            </Row>
-            : null 
-          }
-          <div className="mentor-detail-myself">
-            {!this.state.more && (description.length > 200 ? <p>{description.slice(0,200)}...</p> : <p>{description}</p>)}
-            {this.state.more && <p>{description}</p>}
-            {description.length > 200 && (this.state.more ? <a href="javascript:void(0)" className="read-more" onClick={() => this.readLess()}>Read less</a> : <a href="javascript:void(0)" className="read-more" onClick={() => this.readMore()}>Read more</a>)}
+        {loading && <LoadingModal open={true} />}
+        <div className="mentor-detail-card">
+          <div style={{ position: "relative" }} className="mentor-detail-avatar">
+            {avatar && <img src={avatar} alt={name} className="mentor-detail-avatar-img" />}
+            {!avatar && <img src={defaultavatar} alt={name} className="mentor-detail-avatar-img" />}
+            {
+              status === 1 && <div className="carousel-component-online-class"></div>
+            }
           </div>
-          {is_mentor ?
-            <div className="mentor-detail-video">
-              <a href={video_url} target="_blank" rel="noopener noreferrer" ><img src={PlayIcon} alt="play-icon"/>Video presentation</a>
+          <div className="mentor-detail-desc">
+            <Row className="metor-detail-name-score">
+              <div className="mentor-detail-name">{name}</div>
+              {is_mentor ? <div><img src={StarIcon} alt="star-icon" className="mentor-detail-score" />{average_mark}</div> : null}
+            </Row>
+            {is_mentor ?
+              <Row className="mentor-detail-subject-tag">
+                <h5 className="tag-title mentor-detail-subject-title">Teaches: </h5>
+                {tag_name && tag_name.map((teach, idx) => {
+                  if (idx < 3)
+                    return <p key={idx} className="brainsshare-tag" title={teach}>{teach}</p>;
+                  else if (idx === 3)
+                    return <p key={idx}>{tag_name.length - 3} more</p>
+                  else
+                    return <></>;
+                })}
+              </Row>
+              : null
+            }
+            <div className="mentor-detail-myself">
+              {!this.state.more && (description.length > 200 ? <p>{description.slice(0, 200)}...</p> : <p>{description}</p>)}
+              {this.state.more && <p>{description}</p>}
+              {description.length > 200 && (this.state.more ? <a href="javascript:void(0)" className="read-more" onClick={() => this.readLess()}>Read less</a> : <a href="javascript:void(0)" className="read-more" onClick={() => this.readMore()}>Read more</a>)}
             </div>
-            : null
-          }
-        </div>
-        {/* {is_mentor ? */}
+            {is_mentor ?
+              <div className="mentor-detail-video">
+                <a href={video_url} target="_blank" rel="noopener noreferrer" ><img src={PlayIcon} alt="play-icon" />Video presentation</a>
+              </div>
+              : null
+            }
+          </div>
+          {/* {is_mentor ? */}
           <div className="mentor-deatail-rate-buttons">
             <Row className="center">
               <p>
                 $ {hourly_price} / 60 min
               </p>
             </Row>
-            {status ? 
+            {status ?
               <Row className="center">
                 {instant_call ? <Button className="btn-mentor-detail-instant" onClick={() => this.handleInstantLive(id)}>
-                  <img src={Lightening} alt="Lightening"/>
+                  <img src={Lightening} alt="Lightening" />
                   Available now
-                </Button> : <></> }
+                </Button> : <></>}
               </Row>
               : null
             }
             <Row className="center">
-              <Button style={{marginBottom: 10}} className="btn-mentor-detail-book" onClick={() => this.handleBookSession(id)}>
+              <Button style={{ marginBottom: 10 }} className="btn-mentor-detail-book" onClick={() => this.handleBookSession(id)}>
                 <img src={Clock} alt="Clock" />
                 Book a session
               </Button>
             </Row>
           </div>
-        {/* //   : null
+          {/* //   : null
         // } */}
-      </div>
-      {/* {is_mentor ? */}
-        <div style={{paddingRight: 0, paddingLeft: 0, marginBottom: "20px"}}>
+        </div>
+        {/* {is_mentor ? */}
+        <div style={{ paddingRight: 0, paddingLeft: 0, marginBottom: "20px" }}>
           <Card small className="share-page-card">
-            <CardBody style={{paddingLeft: 0}}>
+            <CardBody style={{ paddingLeft: 0 }}>
               <Row>
                 <Col xl="3" className="subscription-mentor-detail">
-                  <div style={{textAlign: 'center'}}>
-                    <h3 style={{fontSize: 30, fontWeight: 'bold', color: '#333333'}}>{name}</h3>
-                    <h3 style={{fontSize: 30, fontWeight: 'bold', color: '#333333'}}>Share Page</h3>
-                    {avatar && <img className="mentor-detail-avatar-img" src={avatar} alt="avatar"/>}
-                    {!avatar && <img className="mentor-detail-avatar-img" src={defaultavatar} alt="avatar"/>}
-                    <div style={{display: "flex", padding: "20px 0px"}}>
-                      <img src={SubscriperImg} style={{width: "22px", marginRight: "10px"}} alt="icon"/>
-                      <h6 className="no-margin" style={{paddingRight: "70px"}}>Subscribers</h6>
-                      <h6 className="no-margin"style={{fontWeight: "bold"}}>{sub_count}</h6>
+                  <div style={{ textAlign: 'center' }}>
+                    <h3 style={{ fontSize: 30, fontWeight: 'bold', color: '#333333' }}>{name}</h3>
+                    <h3 style={{ fontSize: 30, fontWeight: 'bold', color: '#333333' }}>Share Page</h3>
+                    {avatar && <img className="mentor-detail-avatar-img" src={avatar} alt="avatar" />}
+                    {!avatar && <img className="mentor-detail-avatar-img" src={defaultavatar} alt="avatar" />}
+                    <div style={{ display: "flex", padding: "20px 0px" }}>
+                      <img src={SubscriperImg} style={{ width: "22px", marginRight: "10px" }} alt="icon" />
+                      <h6 className="no-margin" style={{ paddingRight: "70px" }}>Subscribers</h6>
+                      <h6 className="no-margin" style={{ fontWeight: "bold" }}>{sub_count}</h6>
                     </div>
-                    <Button className="btn-subscription-unsubscribe" onClick={() => this.toggle_unsubscribe()}>
-                    Subscription ${sub_plan_fee}/month
-                  </Button>
+                    {sub_id.indexOf(parseInt(localStorage.getItem('user_id'))) === -1 ?
+                      <Button className="btn-subscription-unsubscribe" onClick={() => this.toggle_unsubscribe()}>
+                        Subscription ${sub_plan_fee}/month </Button>
+                      : <Button className="btn-subscription-unsubscribe" onClick={() => this.handleUnSub(id)}>
+                        Unsubscription
+                      </Button>
+                    }
                   </div>
-                  
                 </Col>
                 <Col xl="9" lg="12" className="subscription-mentor-videos">
-                  {share_info && share_info.map((item, idx) => 
+                  {share_info && share_info.map((item, idx) =>
                     <MentorVideo key={idx} item={item} />
                   )}
                 </Col>
               </Row>
             </CardBody>
-          </Card>    
+          </Card>
         </div>
         {/* : null
       } */}
-      {/* <SubscribeModal item={this.props.mentorData} open={subscriptionOpen} actionSuccess={this.actionSuccess} toggle_modal={() => this.toggle_modal()} toggle={() => this.toggle_unsubscribe()} /> */}
-      {subscriptionOpen && 
-         <SubscribeModal item={this.props.mentorData} open={subscriptionOpen} actionSuccess={this.actionSuccess} toggle_modal={() => this.toggle_modal()} toggle={() => this.toggle_unsubscribe()} /> 
-      }
-      <AddNewCard toggle={() => this.toggle_addnewcardmodal()} open={this.state.addnewcardModal}></AddNewCard>
+        {/* <SubscribeModal item={this.props.mentorData} open={subscriptionOpen} actionSuccess={this.actionSuccess} toggle_modal={() => this.toggle_modal()} toggle={() => this.toggle_unsubscribe()} /> */}
+        {subscriptionOpen &&
+          <SubscribeModal item={this.props.mentorData} open={subscriptionOpen} actionSuccess={this.actionSuccess} toggle_modal={() => this.toggle_modal()} toggle={() => this.toggle_unsubscribe()} />
+        }
+        <AddNewCard toggle={() => this.toggle_addnewcardmodal()} open={this.state.addnewcardModal}></AddNewCard>
       </>
     );
   }
