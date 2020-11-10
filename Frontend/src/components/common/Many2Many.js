69f9afff7d8310de8f75d2d7b20b1260a4923f83
kritical0613 <kulishan0613@gmail.com>
2020-11-10 22:08:37 +0400

    fixed some issues, clean code.

--- Frontend/src/components/common/Many2Many.js
+++ Frontend/src/components/common/Many2Many.js
@@ -34,10 +34,6 @@ import { MessageList, MessageInput } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
 import 'stream-chat-react/dist/css/index.css';
 
-import ReactNotification from 'react-notifications-component';
-import 'react-notifications-component/dist/theme.css';
-import { store } from 'react-notifications-component';
-
 import { ACCESS_API_KEY, ACCESS_TOKEN_SECRET } from '../../common/config';
 
 var channel;
@@ -1244,57 +1240,6 @@ export default class Many2Many extends React.Component {
     this.setState({
       inviteModal: false, 
     })
-  }
-
-  showSuccess(text) {
-    store.addNotification({
-      title: "Success",
-      message: text,
-      type: "success",
-      insert: "top",
-      container: "top-right",
-      dismiss: {
-        duration: 500,
-        onScreen: false,
-        waitForAnimation: false,
-        showIcon: false,
-        pauseOnHover: false
-      },
-    });
-  }
-
-  showFail(text) {
-    store.addNotification({
-      title: "Fail",
-      message: text,
-      type: "danger",
-      insert: "top",
-      container: "top-right",
-      dismiss: {
-        duration: 500,
-        onScreen: false,
-        waitForAnimation: false,
-        showIcon: false,
-        pauseOnHover: false
-      }
-    });
-  }
-
-  showWarning(text) {
-    store.addNotification({
-      title: "Warning",
-      message: text,
-      type: "warning",
-      insert: "top",
-      container: "top-right",
-      dismiss: {
-        duration: 500,
-        onScreen: false,
-        waitForAnimation: false,
-        showIcon: false,
-        pauseOnHover: false
-      }
-    });
   }
 
   startSessionTimer() {
